6e65ec6f17ab4ee9d752dec0a638493fd25b3326
Lyn <lynfinman@gmail.com>
2022-06-13 12:52:25 -0400

    added cleanup

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,9 +1,26 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
   // add useEffect code
+  useEffect(() => {
+    const timerId = setTimeout(() => {
+      if (timeRemaining > 1) {
+        setTimeRemaining((prevTimeRemaining) => prevTimeRemaining - 1);
+        //console.log("Decremented timeRemaining");
+      } else {
+        //console.log("Set time remaining to 10");
+        setTimeRemaining(10);
+        onAnswered(false);
+      }
+    }, 1000);
+    return () => {
+      console.log("In cleanup.  Clearing timer", timerId);
+      clearTimeout(timerId);
+    };
+    console.log("timerId is ", timerId);
+  }, [timeRemaining, onAnswered]);
 
   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
@@ -11,7 +28,7 @@ function Question({ question, onAnswered }) {
   }
 
   const { id, prompt, answers, correctIndex } = question;
-
+  console.log("Time remaining is", timeRemaining);
   return (
     <>
       <h1>Question {id}</h1>
